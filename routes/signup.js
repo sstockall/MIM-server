d6097b7de315079f76894acc6169e2d1dd69608f
Samantha Stockall <sam.stockall@hotmail.com>
2022-07-17 20:07:39 -0400

    Updating server and lesion page

--- routes/signup.js
+++ routes/signup.js
@@ -3,13 +3,33 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const { v4: uuidv4 } = require('uuid');
 const knex = require('knex')(require('../knexfile'));
+const { check, validationResult } = require('express-validator');
+
+let validations = [
+    check("email")
+      .isEmail()
+      .withMessage("The email you have entered is not valid"),
+  
+    check("password")
+      .isLength({ min: 5 })
+      .withMessage("The password must have at least 5 characters")
+  ];
 
 router.route('/')
-    .post((req, res) => {
+    .post(validations, (req, res) => {
         const { first_name, last_name, email, password } = req.body;
 
         if (!first_name || !last_name || !email || !password) {
             return res.status(400).send("Please enter the required fields.");
+        }
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            const err = {};
+            errors.array().forEach(error => {
+            err[error.param] = error.msg;
+            });
+            return res.status(422).json({ errors: err });
         }
        
         const hashedPassword = bcrypt.hashSync(password, 10);
